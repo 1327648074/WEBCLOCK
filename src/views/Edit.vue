4281b43a21a55ae68ec8af694b2c86f4d69307b4
13276 <1327648074@qq.com>
2020-04-28 22:52:47 +0800

    每日打卡

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,44 +1,41 @@
 <template>
-
-    <el-container >
-        <el-header>
-            <el-menu default-active="activeIndex1" mode="horizontal" background-color="#0080ff" text-color="#000" active-text-color="#fff"  >
-                <el-menu-item index="0" disabled>欢迎来到打卡系统</el-menu-item>
-                <el-menu-item index="1">社区</el-menu-item>
-                <el-menu-item index="2" >用户名</el-menu-item>
-                <el-menu-item index="3">个人空间</el-menu-item>
-                <el-menu-item index="4">注销</el-menu-item>
-            </el-menu>
-        </el-header>
-        <br/>
+    <div class="myEdit">
         <el-container>
-            <el-aside style="height: 600px;width:500px">
-                <el-tag tabindex="0" color="#42b983" effect="dark">项目名称:  </el-tag><br/>
-                <el-tag tabindex="1" color="#42b983" effect="dark">时间周期:  </el-tag><br/>
-                <el-tag tabindex="2" color="#42b983" effect="dark">目    的:  </el-tag><br/>
-                <el-tag tabindex="3" color="#42b983" effect="dark">计划时间:  </el-tag><br/>
-                <el-tag tabindex="4" color="#42b983" effect="dark">已打卡天数:</el-tag>
-            </el-aside>
+            <el-header>
+                <el-menu class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                    <el-menu-item index="1">欢迎来到打卡系统</el-menu-item>
+                    <el-menu-item index="2">用户名</el-menu-item>
+                    <el-menu-item index="3" disabled>个人中心</el-menu-item>
+                    <el-menu-item index="4"><a href="">注销</a></el-menu-item>
+                </el-menu>
+            </el-header>
+            <br/>
+            <el-container>
+                <el-aside style="height: 600px;width:500px">
+                    <el-tag tabindex="0" color="#42b983" effect="dark">项目名称:  </el-tag><br/>
+                    <el-tag tabindex="1" color="#42b983" effect="dark">时间周期:  </el-tag><br/>
+                    <el-tag tabindex="2" color="#42b983" effect="dark">目    的:  </el-tag><br/>
+                    <el-tag tabindex="3" color="#42b983" effect="dark">计划时间:  </el-tag><br/>
+                    <el-tag tabindex="4" color="#42b983" effect="dark">已打卡天数:</el-tag>
+                </el-aside>
 
-            <el-main style="height: 800px">
-                <el-calendar id="calendar">
-                    <template
-                            slot="dateCell"
-                            slot-scope="{date, data}">
-                        <div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
-                        <div v-if="data.day.split('-').slice(1)[0] == value" > //判断显示当前页，value是显示当前月份
+                <el-main style="height: 800px">
+                    <el-calendar id="calendar">
+                        <template
+                                slot="dateCell"
+                                slot-scope="{date, data}">
+                            <div class="calendar-day">{{ data.day.split('-').slice(2).join('-') }}</div>
+                            <div v-if="data.day.split('-').slice(1)[0] == value" > //判断显示当前页，value是显示当前月份
 
-                        </div>
-                    </template>
-                </el-calendar>
-                <el-button type="success" @click="clock">今日打卡</el-button>
-                <el-button type="danger" @click="cancel">取消</el-button>
-            </el-main>
+                            </div>
+                        </template>
+                    </el-calendar>
+                    <el-button type="success" @click="clock">今日打卡</el-button>
+                    <el-button type="danger" @click="cancel">取消</el-button>
+                </el-main>
+            </el-container>
         </el-container>
-    </el-container>
-
-
-
+    </div>
 </template>
 
 <script>
@@ -103,4 +100,27 @@
         top:50px
     }
 
+    .is-selected {
+        color: #1989FA;
+    }
+    .el-header {
+        color: #333;
+        background-color:#FFFFFF;
+        text-align: center;
+        line-height: 0px;
+    }
+    .myEdit /deep/ .el-menu-item {
+        color: #333;
+        text-align: center;
+        line-height: 50px;
+        padding: 0 135px;
+    }
+    .myEdit /deep/ .el-main{
+        width:1000px;
+        height:825px;
+        position: relative;
+        top: 0px;
+        left: 0px;
+        padding:0 0px;
+    }
 </style>
